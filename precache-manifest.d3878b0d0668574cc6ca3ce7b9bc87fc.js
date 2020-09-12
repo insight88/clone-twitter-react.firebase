519a63342ea933db286a235ac0491787d278c848
insight88 <kpkim1988@gmail.com>
2020-09-12 11:29:36 +0900

    Updates

---
+++ precache-manifest.d3878b0d0668574cc6ca3ce7b9bc87fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb366401337a640ff85153e34d45fd19",
+    "url": "/clone-twitter-react.firebase/index.html"
+  },
+  {
+    "revision": "09f8b76b8e87de0faa7b",
+    "url": "/clone-twitter-react.firebase/static/css/main.86fde8f1.chunk.css"
+  },
+  {
+    "revision": "2be805f9f60fbdd9ff86",
+    "url": "/clone-twitter-react.firebase/static/js/2.d5af0c83.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/clone-twitter-react.firebase/static/js/2.d5af0c83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09f8b76b8e87de0faa7b",
+    "url": "/clone-twitter-react.firebase/static/js/main.01b19a7a.chunk.js"
+  },
+  {
+    "revision": "faf826e2bcf865737273",
+    "url": "/clone-twitter-react.firebase/static/js/runtime-main.8e08be56.js"
+  }
+]);
